5038bebd0a24f6e43796de708336d3859060a0c8
Danillo Santos <danillobatista22@gmail.com>
2020-12-09 22:51:05 -0300

    Implementando compra de ingresso

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -15,12 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Carrinho;
 import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 import br.com.caelum.ingresso.model.form.SessaoForm;
 import br.com.caelum.ingresso.rest.OmdbClient;
 import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
@@ -34,9 +35,12 @@ public class SessaoController {
 
 	@Autowired
 	private SessaoDao sessaoDao;
-	
+
 	@Autowired
 	private OmdbClient client;
+
+	@Autowired
+	private Carrinho carrinho;
 
 	@GetMapping("/admin/sessao")
 	public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form) {
@@ -44,6 +48,7 @@ public class SessaoController {
 		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
 
 		modelAndView.addObject("sala", salaDao.findOne(salaId));
+		modelAndView.addObject("carrinho", carrinho);
 		modelAndView.addObject("filmes", filmeDao.findAll());
 		modelAndView.addObject("form", form);
 
@@ -75,10 +80,12 @@ public class SessaoController {
 		ModelAndView modelAndView = new ModelAndView("sessao/lugares");
 
 		Sessao sessao = sessaoDao.findOne(sessaoId);
+
 		Optional<ImagemCapa> imagemCapa = client.request(sessao.getFilme(), ImagemCapa.class);
 
 		modelAndView.addObject("sessao", sessao);
 		modelAndView.addObject("imagemCapa", imagemCapa.orElse(new ImagemCapa()));
+		modelAndView.addObject("tiposDeIngressos", TipoDeIngresso.values());
 
 		return modelAndView;
 	}
--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -1,29 +1,77 @@
 package br.com.caelum.ingresso.model;
 
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-import br.com.caelum.ingresso.model.descontos.Desconto;
-
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+@Entity
 public class Ingresso {
 
-	private	Sessao sessao;
-	private	BigDecimal preco;
-	public Ingresso(Sessao	sessao,	Desconto tipoDeDesconto) {
-		this.sessao	= sessao;
-		this.preco	= tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne
+	private Sessao sessao;
+
+	private BigDecimal preco;
+
+	@ManyToOne
+	private Lugar lugar;
+
+	@Enumerated(EnumType.STRING)
+	private TipoDeIngresso tipoDeIngresso;
+	
+	public	Ingresso(){
 	}
+
+
+	public Ingresso(Sessao sessao, TipoDeIngresso tipoDeIngresso, Lugar lugar) {
+		this.sessao = sessao;
+		this.tipoDeIngresso = tipoDeIngresso;
+		this.preco = tipoDeIngresso.aplicaDesconto(sessao.getPreco());
+
+		this.lugar = lugar;
+
+	}
+
 	public BigDecimal getPreco() {
 		return preco.setScale(2, RoundingMode.HALF_UP);
 	}
-	public void setPreco(BigDecimal	preco) {
+
+	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
+
 	public Sessao getSessao() {
 		return sessao;
 	}
+
 	public void setSessao(Sessao sessao) {
 		this.sessao = sessao;
 	}
-	
+
+	public Lugar getLugar() {
+		return lugar;
+	}
+
+	public void setLugar(Lugar lugar) {
+		this.lugar = lugar;
+	}
+
+	public TipoDeIngresso getTipoDeIngresso() {
+		return tipoDeIngresso;
+	}
+
+	public void setTipoDeIngresso(TipoDeIngresso tipoDeIngresso) {
+		this.tipoDeIngresso = tipoDeIngresso;
+	}
+
 }
